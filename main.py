0ff42f1a4f906fddbd0c8a3f30ddf1c61805c285
Ted Personal <tedpersonal@TeddeMacBook-Pro.local>
2023-05-31 15:02:15 +0800

    feat: load notion secret key

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from crawler import Crawler
+from dotenv import load_dotenv
+import os
 
 END_POINT = "https://rate.bot.com.tw/xrt?Lang=en-US"
 
 if __name__ == "__main__":
+    load_dotenv("./local.env")
     spider = Crawler()
     spider._get_exchange_rate_response(END_POINT)
     usd_rate = spider._parse_usd_spot_buying_rate()
+    secret = os.getenv("NOTION_SECRET")
